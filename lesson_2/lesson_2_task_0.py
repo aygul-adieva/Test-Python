72a3e4248bac03407b6bb4a75c41add82b42f213
aygul-adieva <aygul-adieva@yandex.ru>
2024-04-04 22:07:55 +0700

    add second homework

---
+++ lesson_2/lesson_2_task_0.py
@@ -0,0 +1,5 @@
+my_age = int(input("Введите ваш возраст: "))
+print(f"Ваш возраст:", {my_age})
+my_age = my_age + 1
+print(f"Обновленный возраст:" {my_age})
+my_age = int(input("Введите ваш возраст: "))
